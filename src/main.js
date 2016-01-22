591892dd64faeacff5d8263641f6d938ffd257f6
moshilos <moshi.los@gmail.com>
2016-01-22 15:30:52 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by moshi on 1/22/2016.
+ */
+console.log('333423');
+document.write('gsdgsd');
+
+
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+/**
+ * Created by moshi on 1/22/2016.
+ */
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        path: './build/js',
+        filename: "app.js"
+    },
+    module: {
+        loaders: [
+            {
+                // HTML LOADER
+                // Reference: https://github.com/webpack/raw-loader
+                // Allow loading html through js
+                test: /\.html$/,
+                loader: 'raw'
+            },
+            {
+                test: /\.styl$/,
+                loader: 'style-loader!css-loader!stylus-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            }
+        ]
+    },
+    'html-minify-loader': {
+        empty: true,        // KEEP empty attributes
+        cdata: true,        // KEEP CDATA from scripts
+        comments: true     // KEEP comments
+    }
+};
+console.log(module.exports);
